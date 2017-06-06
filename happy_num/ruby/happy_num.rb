4843c2137069ba3e9b667c9ac60e7419229a52c2
Robert Gill <locke@sdf.lonestar.org>
2017-06-05 23:14:53 -0700

    Add Ruby implementation of Happy Numbers

---
+++ happy_num/ruby/happy_num.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/ruby
+#
+# Created:  Mon 05 Jun 2017 10:55:34 PM PDT
+# Modified: Mon 05 Jun 2017 11:06:42 PM PDT
+#
+# Find the first 8 happy numbers
+# Author: Robert Gill <locke@sdf.lonestar.org>
+# Copyright 2017 (C) Robert Gill
+#   All rights reserved.
+#
+
+class Integer
+  def happy?
+    num = self
+    loop do
+      num = sum_sqr_digits(num)
+      break if num == 1 || num < 10
+    end
+    num == 1
+  end
+
+  private
+
+  def sum_sqr_digits(num)
+    sum = 0
+    while num > 0
+      digit = num % 10
+      sum += digit * digit
+      num /= 10
+    end
+    sum
+  end
+end
+
+found = 0
+2.step.each do |n|
+  if n.happy?
+    found += 1
+    puts "#{n} is a Happy Number!"
+  end
+  break if found == 8
+end
