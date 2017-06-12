bde09be74bbcec632989dffe249a100c651b4747
Robert Gill <locke@sdf.lonestar.org>
2017-06-12 13:32:58 -0700

    happy_num.scm: rename happyp to happy?

--- numbers/happy_num/scheme/happy_num.scm
+++ numbers/happy_num/scheme/happy_num.scm
@@ -1,6 +1,6 @@
 ;
 ; Created:  Fri 02 Jun 2017 12:36:50 PM PDT
-; Modified: Sat 10 Jun 2017 04:20:29 PM PDT
+; Modified: Mon 12 Jun 2017 01:31:22 PM PDT
 ;
 ; Copyright 2017 (C) Robert Gill
 ;
@@ -15,16 +15,16 @@
 	(sum-sqr-digits-r (quotient num 10)(+ sum (* digit digit))))))
   (sum-sqr-digits-r num 0))
 
-(define (happyp num)
+(define (happy? num)
   (let ((num (sum-sqr-digits num)))
     (if (and (not (eq? num 1)) (> num 9))
-      (happyp num)
+      (happy? num)
       (eq? num 1))))
 
 (define (list-happy-nums start max)
   (define (list-happy-nums-r num count)
     (if (>= count max) '()
-      (if (happyp num)
+      (if (happy? num)
 	(begin (display num) (display " is a Happy Number!")
 	       (newline)
 	       (list-happy-nums-r (1+ num) (1+ count)))
